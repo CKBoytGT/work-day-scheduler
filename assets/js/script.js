3caca44138281817fee10274d4cba2ba15d9c7a1
C K Boyt <ckboyt@gmail.com>
2023-02-01 20:00:33 -0500

    Add acceptance message and new styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,15 @@ $( document ).ready(function() {
     const text = $(this).siblings(".description").val();
 
     // store text in local storage under the hour-# id
-    localStorage.setItem(time, text)
+    localStorage.setItem(time, text);
+
+    // display the "appointment added" message
+    $('#apptAdded').removeClass('d-none');
+
+    // hide the message after 2.5 seconds
+    setTimeout(function() {
+      $('#apptAdded').addClass('d-none');
+    }, 2500);
   });
 
   // for each time block that exists on the page
@@ -17,7 +25,7 @@ $( document ).ready(function() {
     // get the just the numerical hour of the time block from the id by removing "hour-"
     const blockHour = ($(this).attr('id')).replace("hour-", "");
     // get the current hour in 24 hr time
-    const currentHour = dayjs().hour();
+    const currentHour = 12; //dayjs().hour();
 
     // change the time block's class depending on the current hour
     if (blockHour < currentHour) {
@@ -36,10 +44,8 @@ $( document ).ready(function() {
     const parentId = $(this).parent().attr('id');
     const storedText = localStorage.getItem(parentId);
 
-    // display the text that has been stored under the parent's id (if any)
-    if (storedText) {
-      $(this).val(storedText);
-    }
+    // display the text that has been stored under the parent's id
+    $(this).val(storedText);
   });
 
   // displays the current date in the header of the page.
